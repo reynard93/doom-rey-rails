1d1fbf084936451e615b0484e1c035610c468f32
reynard93 <reynard.lee@thoughtworks.com>
2024-07-08 13:54:42 +0800

    add dape config

--- user/config.el
+++ user/config.el
@@ -142,7 +142,7 @@
 ;; Change Javascript autoformat
 (setq-hook! 'rjsx-mode-hook +format-with 'prettier)
 (setq-hook! 'typescript-tsx-mode-hook +format-with 'prettier)
-(setq-hook! 'typescript-mode-hook +format-with 'prettier) 
+(setq-hook! 'typescript-mode-hook +format-with 'prettier)
 ;; read My new keybinds don't https://github.com/doomemacs/doomemacs/blob/35865ef5e89442e3809b8095199977053dd4210f/docs/faq.org#my-new-keybinds-dont-work
 
 (defun yabai-move-on-error (direction move-fn)
@@ -237,3 +237,54 @@
 (use-package! unity
   :defer t
   :config (add-hook 'after-init-hook #'unity-mode))
+
+;; (map!
+;;  :map dap-mode-map
+;;  :leader
+;;  (:prefix ("d" . "dap")
+;;   ;; basics
+;;   :desc "dap next"          "n" #'dap-next
+;;   :desc "dap step in"       "i" #'dap-step-in
+;;   :desc "dap step out"      "o" #'dap-step-out
+;;   :desc "dap continue"      "c" #'dap-continue
+;;   :desc "dap hydra"         "h" #'dap-hydra
+;;   :desc "dap debug restart" "r" #'dap-debug-restart
+;;   :desc "dap debug"         "s" #'dap-debug
+
+;;   ;; debug
+;;   :prefix ("dd" . "Debug")
+;;   :desc "dap debug recent"  "r" #'dap-debug-recent
+;;   :desc "dap debug last"    "l" #'dap-debug-last
+
+;;   ;; eval
+;;   :prefix ("de" . "Eval")
+;;   :desc "eval"                "e" #'dap-eval
+;;   :desc "eval region"         "r" #'dap-eval-region
+;;   :desc "eval thing at point" "s" #'dap-eval-thing-at-point
+;;   :desc "add expression"      "a" #'dap-ui-expressions-add
+;;   :desc "remove expression"   "d" #'dap-ui-expressions-remove
+
+;;   :prefix ("db" . "Breakpoint")
+;;   :desc "dap breakpoint toggle"      "b" #'dap-breakpoint-toggle
+;;   :desc "dap breakpoint condition"   "c" #'dap-breakpoint-condition
+;;   :desc "dap breakpoint hit count"   "h" #'dap-breakpoint-hit-condition
+;;   :desc "dap breakpoint log message" "l" #'dap-breakpoint-log-message))
+
+(use-package! dape
+  :config
+  ;; Kill compile buffer on build success
+  (add-hook 'dape-compile-hook 'kill-buffer)
+  ;; Projectile users
+  (setq dape-cwd-fn 'projectile-project-root)
+  (setq dape-buffer-window-arrangement 'gud)
+  ;; Projectile users
+  ;; (setq dape-cwd-fn 'projectile-project-root)
+  )
+
+(add-to-list 'dape-configs
+             `(rdbg-attach-rails
+               ;; prefix-local "/Users/reynardtw/formflow-mono/packages/app/"
+               ;; prefix-remote "/usr/app/"
+               port 5678
+               :request "attach"
+               ))
